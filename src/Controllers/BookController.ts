13fdbe17aa656b7087d234bd5546bc23004555e0
ManuelTello <manuelTello.business@gmail.com>
2022-07-20 16:08:24 -0300

    Render de vistas agregado al controller

--- src/Controllers/BookController.ts
+++ src/Controllers/BookController.ts
@@ -2,6 +2,7 @@ import express, { Router, Request, Response } from "express";
 import IController from "../Interfaces/IController";
 import Book from "../Models/Book";
 import { BookRepo } from "../Repos/BookRepo";
+import { IView } from "../Interfaces/IView";
 
 class BookController implements IController
 {
@@ -17,33 +18,47 @@ class BookController implements IController
     {
         this._Router.get(`${this._URLPath}/index`,this.Index)
                     .get(`${this._URLPath}/index/:id`,this.Index);
-        this._Router.post(`${this._URLPath}/add`,this.Add);
+        this._Router.get(`${this._URLPath}/add`,this.AddView)
+                    .post(`${this._URLPath}/add`,this.Add);
     }
 
     private Index(req:Request,res:Response):void
     {
         const id:string = req.params.id;
         const repo:BookRepo = new BookRepo();
-        const books:Book[] = id ?  repo.GetBooks(id) : repo.GetBooks();
-
-        res.status(200).json(books);
+        const payload:Book[] = id ?  repo.GetBooks(id) : repo.GetBooks();
+        
+        const view:IView = 
+        {
+            Base:"base",
+            Data:{Title:"Index of Books",Controller:"Books",Action:"Index",Payload:payload}
+        }
+        res.render(view.Base,view.Data);
     }
+
     
     private Add(req:Request,res:Response):void
     {
-        const id:string = req.body.id;
+        console.log("Added");
+        /*const id:string = req.body.id;
         const title:string = req.body.title;
         const author:string = req.body.author;
         const published:Date = req.body.published;
 
         const book:Book = new Book(id,title,author,published);
         new BookRepo().AddBook(book);
-        res.status(200).redirect(`${this._URLPath}/index`);
+        res.status(200).redirect(`${this._URLPath}/index`);*/
     }
 
     private AddView(req:Request, res:Response):void
     {
+        const view:IView =
+        {
+            Base:"base",
+            Data:{Controller:"Books",Action:"Add",Title:"Add Book"}
+        };
 
+        res.status(200).render(view.Base,view.Data);
     }
 }
 
